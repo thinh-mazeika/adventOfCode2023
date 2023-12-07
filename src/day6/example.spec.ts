3f97d995753662e6b988d2486ff2e9f350efabf2
thinh-mazeika <thinh.mazeika@beckshybrids.com>
2023-12-06 23:57:07 -0500

    day 6 part 2

--- src/day6/example.spec.ts
+++ src/day6/example.spec.ts
@@ -1,9 +1,17 @@
 import * as fs from 'fs'
 import calculatePossibleWinningWays from './solution'
+import calculatePossibleWinningWaysPart2 from './solution-part2'
 
 test('day6_part1 example solution will be 288', () => {
   const inputFile = fs.readFileSync('src/day6/example.txt', 'utf8')
   const data: string[] = inputFile.split('\n')
   const result = calculatePossibleWinningWays(data)
   expect(result).toBe(288)
+})
+
+test('day6_part2 example solution will be 71503', () => {
+  const inputFile = fs.readFileSync('src/day6/example.txt', 'utf8')
+  const data: string[] = inputFile.split('\n')
+  const result = calculatePossibleWinningWaysPart2(data)
+  expect(result).toBe(71503)
 })
--- src/day6/solution.spec.ts
+++ src/day6/solution.spec.ts
@@ -1,9 +1,17 @@
 import * as fs from 'fs'
 import calculatePossibleWinningWays from './solution'
+import calculatePossibleWinningWaysPart2 from './solution-part2'
 
 test('day6_part1 puzzle solution will be 131376', () => {
   const inputFile = fs.readFileSync('src/day6/puzzle.txt', 'utf8')
   const data: string[] = inputFile.split('\n')
   const result = calculatePossibleWinningWays(data)
   expect(result).toBe(131376)
+})
+
+test('day6_part2 puzzle solution will be 34123437', () => {
+  const inputFile = fs.readFileSync('src/day6/puzzle.txt', 'utf8')
+  const data: string[] = inputFile.split('\n')
+  const result = calculatePossibleWinningWaysPart2(data)
+  expect(result).toBe(34123437)
 })
