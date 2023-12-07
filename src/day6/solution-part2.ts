980dca49860cda5fa9ee81f42df8f8155c1b4775
thinh-mazeika <thinh.mazeika@beckshybrids.com>
2023-12-06 23:56:27 -0500

    day 6 part 2

--- src/day6/solution-part2.ts
+++ src/day6/solution-part2.ts
@@ -1,13 +1,37 @@
-import * as fs from 'fs'
+type TimeAndDistanceRecord = {
+  timeRecord: number
+  distanceRecord: number
+  possibleWinningWays: number
+}
 
-const inputFile = fs.readFileSync('src/day6/puzzle.txt', 'utf8').split('\n')
-const timeRecords = inputFile[0].split(':')[1].trim().split(' ').join('')
-console.log(timeRecords, typeof timeRecords, timeRecords.length)
-// swap the last digit of timeRecords with the digit next to the last digit
+function buildRecordObject(inputFile: string[]): TimeAndDistanceRecord[] {
+  const timeRecords = inputFile[0].split(':')[1].trim().split(' ').join('')
+  const distanceRecords = inputFile[1].split(':')[1].trim().split(' ').join('')
 
-const timeRecordsArray = timeRecords.split('')
-const lastDigit = timeRecordsArray[timeRecordsArray.length - 1]
-const secondLastDigit = timeRecordsArray[timeRecordsArray.length - 2]
-timeRecordsArray[timeRecordsArray.length - 1] = secondLastDigit
-timeRecordsArray[timeRecordsArray.length - 2] = lastDigit
-console.log(timeRecordsArray.join(''))
+  return [
+    {
+      timeRecord: +timeRecords,
+      distanceRecord: +distanceRecords,
+      possibleWinningWays: 0,
+    },
+  ]
+}
+
+function calculatePossibleWinningWaysPart2(inputFile: string[]) {
+  const timeAndDistanceRecords = buildRecordObject(inputFile)
+
+  Object.values(timeAndDistanceRecords).forEach((record) => {
+    const { timeRecord, distanceRecord } = record
+
+    for (let i = 1; i <= timeRecord - 1; i++) {
+      if ((timeRecord - i) * i > distanceRecord) {
+        record.possibleWinningWays += 1
+      }
+    }
+  })
+
+  // sum of all possible winning ways
+  return timeAndDistanceRecords.map((record) => record.possibleWinningWays).reduce((a, b) => a + b)
+}
+
+export default calculatePossibleWinningWaysPart2
